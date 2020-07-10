a82d7d77a5888ef9c88666a6f0050b96df2972f8
Dustin <p4569zz@gmail.com>
2020-07-10 19:20:33 +0900

    Initial Commit

---
+++ 20200710_1st/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20200710_1st
+//
+//  Created by Dustin on 2020/07/10.
+//  Copyright © 2020 Dustin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
